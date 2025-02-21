86fed3c8e90170f195f2fd850f71fc7474372d8c
Phoenix <thephoenixproject208@gmail.com>
2025-02-21 14:00:15 +0000

    SCPSwap Stuff

---
+++ EnhancedGameplay/EnhancedGameplayPluginCore.cs
@@ -0,0 +1,28 @@
+﻿using RedRightHandCore.PluginCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnhancedGameplay
+{
+	public class EnhancedGameplayPluginCore : CustomPluginCore
+	{
+		public override string Name => "Enhanced Gameplay";
+
+		public override string Description => "A bunch of misc. changes made to gameplay for our servers";
+
+		public override Version Version => new(0, 0, 0, 1);
+
+		public override void Disable()
+		{
+			throw new NotImplementedException();
+		}
+
+		public override void Enable()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
